ae746310172621b6c2724bf9de2657904d216f4f
lucas <lcsgborges@gmail.com>
2025-04-27 13:02:03 -0300

    check if n and its double exists

---
+++ ex004.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkIfExist(self, arr: list[int]) -> bool:
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                if i != j and arr[i] == 2 * arr[j]:
+                    return True
+        return False
+    
+solution = Solution()
+print(solution.checkIfExist([10,2,5,3]))
