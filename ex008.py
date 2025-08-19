6e02674fdeb17b69b5beda66bb9ecedf0918a17b
lcsgborges <lcsgborges@gmail.com>
2025-08-19 14:06:24 -0300

    mysqrt(x) (easy)

---
+++ ex008.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left, right = 1, x
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
+            
+solution = Solution()
+print(solution.mySqrt(4))
